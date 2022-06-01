6d8a3cdf8d25d059a8942ceabec2cb6a6d3fb1bc
brian ambundo <brian.katiechi9@gmail.com>
2022-06-01 19:58:35 +0300

    commit tree 3b49d8a6a5ec6c9d2036f1d075e53b7dab24e3fd

---
+++ index.js
@@ -0,0 +1,55 @@
+function add(){
+
+}
+
+function subtract(){
+
+}
+
+function multiply(){
+
+}
+
+function divide(){
+
+}
+
+function add(a, b){
+    return a+b
+}
+console.log(add(3,4))
+
+function subtract(a, b){
+    return a-b
+}
+console.log(subtract(3,4))
+
+function multiply(a, b){
+    return a*b
+}
+console.log(multiply(3,4))
+
+function divide(a, b){
+    return a/b
+}
+console.log(divide(3,4))
+
+function increment(n){
+    return ++n
+}
+console.log(increment(42))
+
+function decrement(n){
+    return --n
+}
+console.log(decrement(77))
+
+function makeInt(n){
+   return parseInt(n,10) 
+}
+console.log(makeInt("coder"))
+
+function preserveDecimal(n){
+    return parseFloat(n,10) 
+ }
+ console.log(preserveDecimal("coder"))
